e54f9387272ed1a0f9faddfb3193d73b7976b71f
hashisato <satoki1hashimoto@gmail.com>
2024-04-07 16:59:03 +0900

    GameOverとRetryのボタンを作りました

---
+++ Assets/VRBiribiribou/Scripts/ButtonGameOver.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtonGameOver : MonoBehaviour
+{
+
+    public void OnPushedButton()
+    {
+        
+        SceneManager.LoadScene("GameOverScene"); //ゲームオーバーのシーン（GameOmerScene）に移動する
+    }
+}
---
+++ Assets/VRBiribiribou/Scripts/ButtonRetry.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtonRetry : MonoBehaviour
+{
+
+    public void OnPushedButton()
+    {
+        
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name); //Œ»İ‚ÌƒV[ƒ“‚ğ‚à‚¤ˆê“x“Ç‚İ‚Ş
+    }
+}
