23add047c050b1bf24114bcdb12eb65dfe51200b
ra-yudayon <134184498+ra-yudayon@users.noreply.github.com>
2024-04-12 00:31:57 +0900

    移動する床のコース
    終わりのほうの難易度調整がわかりませんでした。
    動く床についてるスクリプトのpublic float changeDirectionInterval　をすべて任意の値、倍にすれば
    スピード変更可能です

--- Assets/VRBiribiribou/Scripts/MovingCubeScripts/Move.cs
+++ Assets/VRBiribiribou/Scripts/MovingCubeScripts/Move.cs
@@ -6,7 +6,9 @@ public class Move : MonoBehaviour
     public float speed = 1f; // 移動速度
     public float changeDirectionInterval1 = 5f; // 移動方向を変更する間隔
     public float changeDirectionInterval2 = 10f;
-    public float changeDirectionInterval2 = 15f;
+    public float changeDirectionInterval3 = 15f;
+    public float changeDirectionInterval4 = 20f;
+    public float changeDirectionInterval5 = 25f;
     private string movingVector = "forward"; // 床の移動方向
     private float timer = 0f; // 経過時間を計測するタイマー
 
@@ -17,34 +19,63 @@ public class Move : MonoBehaviour
         timer += Time.deltaTime;
 
         // 一定時間が経過したら移動方向を変更する
-        if (timer >= changeDirectionInterval1)
+        if(timer>=changeDirectionInterval5){
+            movingVector = "up";
+        }
+        else if(timer >= changeDirectionInterval4){
+            movingVector ="right";
+        }
+        else if (timer >= changeDirectionInterval3)
         {
-            movingVector = "right"; // 移動方向を反転させる
-        }else if (timer >= changeDirectionInterval2)
+            movingVector = "down"; // 移動方向を下に変更
+        }
+        else if (timer >= changeDirectionInterval1)
         {
-            movingVector = "left";
-        }else if (timer >= changeDirectionInterval3)
-        {
-            movingVector = "down";
+            movingVector = "right"; // 移動方向を右に変更
         }
 
 
         // 床を移動させる
-        if (movingVector == "forward"){
+        if (movingVector == "forward")
+        {
             transform.Translate(Vector3.forward * speed * Time.deltaTime);
             if (player != null)
             {
-
                 player.transform.Translate(Vector3.forward * speed * Time.deltaTime);
             }
-        }else if(movingVector == "right"){
+        }
+        else if (movingVector == "right")
+        {
             transform.Translate(Vector3.right * speed * Time.deltaTime);
             if (player != null)
             {
                 player.transform.Translate(Vector3.right * speed * Time.deltaTime);
             }
         }
-
+        else if (movingVector == "left")
+        {
+            transform.Translate(Vector3.left * speed * Time.deltaTime);
+            if (player != null)
+            {
+                player.transform.Translate(Vector3.left * speed * Time.deltaTime);
+            }
+        }
+        else if (movingVector == "down")
+        {
+            transform.Translate(Vector3.down * speed * Time.deltaTime);
+            if (player != null)
+            {
+                player.transform.Translate(Vector3.down * speed * Time.deltaTime);
+            }
+        }
+        else if (movingVector == "up")
+        {
+            transform.Translate(Vector3.up * speed * Time.deltaTime);
+            if (player != null)
+            {
+                player.transform.Translate(Vector3.down * speed * Time.deltaTime);
+            }
+        }
+        
     }
-
 }
